ab6c5ad68c8614d13d89c8501552492930fd783e
chrisritterc9 <critter@specinno.com>
2015-05-25 13:52:51 -0400

    Update mytest.java

--- mytest.java
+++ mytest.java
@@ -1,12 +1,9 @@
 class Action {
   public void doIt() {}
   public void doMore() {
-<<<<<<< HEAD
     String a = "hello".toString();
     System.currentStackTrace();
     System.out.println(a);
-=======
     System.out.println("OK");
->>>>>>> cd3b31c479e30015e9945eb86bd0c1d9e14e0661
   }
 }
